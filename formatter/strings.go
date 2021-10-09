aa49d095ff449926da695e01305bbd6f2a92585c
Jeremiah <jmiller.v@gmail.com>
2021-10-09 11:47:40 -0600

    feat(formattter): implement StructToString function

--- formatter/strings.go
+++ formatter/strings.go
@@ -1,8 +1,23 @@
 package formatter
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+)
 
+// InterfaceToString returns a string after being parsed by fmt.Sprintf
+// I've noticed that it doesn't work on all structs passed in.
 func InterfaceToString(i interface{}) string {
 	s := fmt.Sprintf("%v", i)
 	return s
+}
+
+// StructToString marshalls a struct into bytes and then outputs them as a string
+func StructToString(i interface{}) string {
+	structBytes, err := json.Marshal(i)
+	if err != nil {
+		log.WithError(err).Error("unable to marhsal struct into bytes")
+	}
+	return string(structBytes)
 }
